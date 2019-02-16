dd348e187e200f5e47ad1005f8c6d0a506004872
Hubert Szymański <hubertszymanski5@gmail.com>
2019-02-16 19:20:35 +0100

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# script to compile project, run it
+# to clean simply go to build and remove files there
+
 cd build
 cmake ..
 make
 ./project1
-rm -r *
+# rm -r *
 echo "Zakonczono"
